346716b6d4d1adfb6cc5ebb68084fd6c8a6a8496
SimplyAMan <simplyaman@gmail.com>
2017-03-13 07:34:54 +0200

    Constructor done

--- src/com/tasks3/carddeck/Deck.java
+++ src/com/tasks3/carddeck/Deck.java
@@ -1,5 +1,19 @@
 package com.tasks3.carddeck;
+
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> list =  new ArrayList<Card>(36);
+
+    public Deck() {
+        int suitLength = Suit.values.length;
+        int rankLength = Rank.values.length;
+        for (int i=0; i < suitLength; i++) {
+            for (int j = 0; j < rankLength; j++) {
+                list.add(new Card(Rank.values[j],Suit.values[i]));
+            }
+        }
+    }
 
     //Перемішує колоду у випадковому порядку
     public void shuffle() {
@@ -24,14 +38,19 @@ public class Deck {
 
     //Повертає true у випадку коли в колоді ще доступні карти
     public boolean hasNext() {
-        return true;
+        if(list.size()>0)
+            return true;
+        else
+            return false;
     }
 
     //"Виймає" одну карту з колоди, коли буде видано всі 36 карт повертає null
     //Карти виймаються з "вершини" колоди. Наприклад перший виклик видасть SPADES 6 потім
     //SPADES 7, ..., CLUBS 6, ..., CLUBS Ace і так далі до HEARTS Ace
     public Card drawOne() {
-        Card card = new Card(new Rank("6"),new Suit("CLUBS"));
+        int size = list.size();
+        Card card = list.get(size - 1);
+        list.remove(size - 1);
         return card;
     }
 }
